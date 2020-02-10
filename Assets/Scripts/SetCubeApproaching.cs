84f30502880e45be8a69127040ef5c413f528829
Barbora Darmová <barbora.darmova@virtuplex.cz>
2020-02-10 18:55:45 +0100

    Initial Commit

---
+++ Assets/Scripts/SetCubeApproaching.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Animator))]
+public class SetCubeApproaching : MonoBehaviour
+{
+    private Animator animator;
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = this.gameObject.GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Cube")
+        {
+            animator.SetBool("CubeApproaching", true);
+        }
+    }
+}
