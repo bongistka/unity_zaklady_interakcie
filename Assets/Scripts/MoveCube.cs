73f20de987c2398b9974d0002a8a636546c431bd
Barbora Darmová <barbora.darmova@virtuplex.cz>
2020-02-10 19:07:00 +0100

    Scriptable movement

--- Assets/Scripts/MoveCube.cs
+++ Assets/Scripts/MoveCube.cs
@@ -4,15 +4,22 @@ using UnityEngine;
 
 public class MoveCube : MonoBehaviour
 {
+    public float speed = 3;
+    public float frequency = 1.0f;
+    public float magnitude = 0.5f;
+
+    private Vector3 pos;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        pos = this.transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        pos += transform.up * Time.deltaTime * speed;
+        transform.position = pos + transform.right * Mathf.Sin(Time.time * frequency) * magnitude;
     }
 }
